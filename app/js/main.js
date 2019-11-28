4abcf42176219b8fe34ad6a4ea2cc0158ce1e173
AlexanderGulyy <alexgulyy80@gmail.com>
2019-11-28 21:08:29 +0200

    added marketplase features weekly

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function(){
 
-	// var mixer = mixitup('.products__inner-box');
+	
+	$(".star-rate").rateYo({
+		rating: 4.5,
+		starWidth: "15px",
+		readOnly: true,
+	});
+
+	$('.weekly-slider').slick({
+
+		prevArrow: '<button type="button" class="slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+		nextArrow: '<button type="button" class="slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+	});
 	
 });
