dd49f541eb99a3f1dbe96641450bcae326f45df0
AlexanderGulyy <alexgulyy80@gmail.com>
2019-12-01 14:44:15 +0200

    added products, followers

--- app/js/main.js
+++ app/js/main.js
@@ -7,10 +7,28 @@ $(function(){
 		readOnly: true,
 	});
 
+	$(".star-rate__product").rateYo({
+		rating: 4.5,
+		starWidth: "13px",
+		readOnly: true,
+		
+
+	});
+
 	$('.weekly-slider').slick({
 
 		prevArrow: '<button type="button" class="slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
 		nextArrow: '<button type="button" class="slick-next"><span class="lnr lnr-chevron-right"></span></button>',
 	});
+
+	$('.followers-slider').slick({
+
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		prevArrow: '<button type="button" class="slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+		nextArrow: '<button type="button" class="slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+	});
+
+	var mixer = mixitup('.products__items');
 	
 });
